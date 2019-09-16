6382d525349841c00cc6b2d57cd4709e890d5629
Luiz Henrique Carvalho <silveira.lh@gmail.com>
2019-09-16 17:54:41 -0300

    DaoSeries

--- Trabalho/Versão 1/Series&MovieList/src/Controller/CadastrarSerieDAO.java
+++ Trabalho/Versão 1/Series&MovieList/src/Controller/CadastrarSerieDAO.java
@@ -1,14 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Controller;
+
+import Model.Serie;
+import java.util.ArrayList;
+import java.util.logging.Logger;
 
 /**
  *
- * @author Massallys
+ * @author Rique
  */
 public class CadastrarSerieDAO {
+        ArrayList<Serie> listaS = new ArrayList(); 
+
+    public void CadastrarSerieDAO(ArrayList<Serie> listaF) {
+        
+    }
     
+    public void CadastrarSerieDAO() {
+        
+    }
+    
+    public void cadastrarSerie(Serie S) throws Exception{
+        this.listaS = listaS;
+        
+        try {
+            int i = 0;
+            
+            for(Serie s : listaS){
+               s.getTituloSerie();
+               s.getFoiAssitida();
+               s.getSendoAssistida();
+               s.getTemporada();
+               
+               listaS.add(s);
+
+               System.out.println(listaS);
+            }
+        } catch (Exception ex) {
+            Logger.getLogger("Erro ao cadastrar a série.");
+        }
+    }
+    
+    public void atualizarFilme(Serie S) throws Exception{
+        //movie.getListaS().add(S);
+    }
+    
+    public void excluirFilme(Serie S) throws Exception{
+        //movie.getListaS().add(S);
+    }
+
 }
